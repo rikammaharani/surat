e86ae3307b1b3f2d41c4150f13b5961b648962c8
rikammaharani <rikammaharani@gmail.com>
2022-09-14 04:20:46 +0700

    commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+
+@section('container')
+<div id="left" class='mt-3'>
+    <h1>About</h1>
+    <img src="img/1931710179.jpg" alt="Rika Maharani" width="200" class="img-thumbnail rounded-circle float-left">
+</div>
+<div id="right">
+    <h5><b>Aplikasi Ini dibuat oleh:</b></h5>
+    <h5>
+        <table>
+            <tr>
+                <td width="100px">Nama</td>
+                <td>: Rika Maharani</td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>: 1931710179</td>
+            </tr>
+            <tr>
+                <td>Tanggal</td>
+                <td>: 14 September 2022</td>
+            </tr>
+        </table>
+    </h5>
+</div>
+
+@endsection
